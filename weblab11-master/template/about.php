61e8f43b24e5b60baaca861f49d50fe939dd7aee
Arul <115526901+sahrul180304@users.noreply.github.com>
2023-12-19 07:47:29 +0700

    Add files via upload

---
+++ weblab11-master/template/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+
+<div class="content">
+    <h2>ini halaman About</h2>
+    <p>ini adalah bagian content dari halaman.</p>
+</div>
+
+<?php require('footer.php'); ?>
---
+++ weblab11-master/template/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet" media="screen' />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Modularisasi Menggunakan Require</h1>
+        </header>
+        <nav>
+            <a href="home.php">home</a>
+            <a href="about.php">tentang</a>
+            <a href="kontak.php">kontak</a>
+        </nav>
